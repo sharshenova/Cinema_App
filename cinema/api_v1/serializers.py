89189a7caccd83ff090e520a1dec10d18ed7f169
Maria Sharshenova <maria@sharshenov.com>
2019-03-04 18:33:57 +0600

    Исправлена ошибка в выводе поля "Категория" в фильмах

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'name', 'description', 'poster', 'release_date', 'finish_date')
+        fields = ('id', 'name', 'description', 'poster', 'release_date', 'finish_date', 'category')
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -15,8 +15,7 @@ class Movie(models.Model):
     poster = models.ImageField(upload_to='posters', null=True, blank=True, verbose_name='Постер')
     release_date = models.DateField(verbose_name='Дата выхода в прокат')
     finish_date = models.DateField(null=True, blank=True, verbose_name='Дата окончания проката')
-    category = models.ForeignKey(Category, null=True, blank=True, related_name='movies_by_category',
-                                 verbose_name='Категория фильма', on_delete=models.PROTECT)
+    category = models.ManyToManyField(Category, blank=True, related_name='movies_by_category', verbose_name='Категория фильма')
 
     def __str__(self):
         return self.name
@@ -36,7 +35,7 @@ class Seat(models.Model):
     place = models.IntegerField(verbose_name='Место')
 
     def __str__(self):
-        return self.place
+        return str(self.place)
 
 
 class Show(models.Model):
