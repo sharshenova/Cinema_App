11e0b82c39683a6cd723e75c8c1c1886fb183d88
Maria Sharshenova <maria@sharshenov.com>
2019-03-07 12:33:45 +0600

    Добавлена пагинация

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+
+# добавлена пагинация
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+}
